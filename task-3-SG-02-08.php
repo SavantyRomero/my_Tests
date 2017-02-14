9d3d7c4a1a425b46206f4d98584ed4c6235ed473
SavantyRomero <melnyk115@gmail.com>
2017-02-14 23:56:44 +0200

    Update task-3-SG-02-08.php

--- task-3-SG-02-08.php
+++ task-3-SG-02-08.php
@@ -15,10 +15,10 @@ $vertical = $_GET['vertical'];
 $two_dimensional_array = array();
 for ($c = 0; $c < $horisontal; $c++):
     for ($r = 0; $r < $vertical; $r++):
-        $two_dimensional_array[$c][$r] = rand(0,PHP_INT_MAX);
+        $two_dimensional_array[$c][$r] = rand(0,2000);
     endfor;
 endfor;
-echo "<table border='1px'>";
+    echo "<table class='locked' border='1px'>";
 for ($c=0;$c<$horisontal;$c++)
 {
     echo "<tr>";
@@ -30,7 +30,27 @@ for ($c=0;$c<$horisontal;$c++)
     }
     echo "</tr>";
 }
-echo "</table>";
+    echo "</table>";
+function if_prime($n){
+    if($n==1||$n==0)return false;
+    for($d=2; $d*$d<=$n; $d++)
+    {
+        if($n%$d==0)return false;
+    }
+    return true;
+}
+
+$prime_array=array();
+for ($i=0;$i<$horisontal;$i++)
+{
+    for($j=0;$j<$vertical;$j++)
+    {
+        if(if_prime($two_dimensional_array[$i][$j])){
+            echo $two_dimensional_array[$i][$j];
+            echo "<br>";
+        }
+    }
+}
 ?>
 </body>
 </html>
